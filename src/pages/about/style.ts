aad725aec0e6e10752dd4fa24119aa4e96484f31
sushant babu prasai <sushantprasaii7@gmail.com>
2023-12-23 18:42:39 +0545

    errors fixed

--- src/pages/about/style.ts
+++ src/pages/about/style.ts
@@ -1,34 +1,27 @@
 import styled from "styled-components";
 
-interface StyledProps {
-    background: string; // Define the background prop
-  }
-  
-
-export const StyledTeam = styled('div')`
-width: 261px;
-height: 406px;
-
-`
+export const StyledTeam = styled("div")`
+  width: 261px;
+  height: 406px;
+`;
 
 export const StyledHeading = styled.h1`
-font-size:2rem;
-font-width:700;
-color: #FFF;
-font-family: Shippori Mincho B1;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-text-align: center; 
-`
-export const StyledParagraph = styled.p<StyledProps>`
-color: rgba(248, 248, 248);
-opacity:0.75;
-text-align: center;
-font-family: Shippori Mincho B1;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-color: ${(props) => props.background || "#fff"};
-`
+  font-size: 2rem;
+  font-width: 700;
+  color: #fff;
+  font-family: Shippori Mincho B1;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+`;
+export const StyledParagraph = styled.p`
+  color: rgba(248, 248, 248);
+  opacity: 0.75;
+  text-align: center;
+  font-family: Shippori Mincho B1;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
