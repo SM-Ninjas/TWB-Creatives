e13f95cd66e824fa87093e63af3db728e960216b
sushant babu prasai <sushantprasaii7@gmail.com>
2023-12-26 18:03:06 +0545

    blogs updated

--- src/pages/blogs/blogs.tsx
+++ src/pages/blogs/blogs.tsx
@@ -1,6 +1,8 @@
+import { StyledImg, StyledDes } from "./style";
+import DOMPurify from 'dompurify';
+
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 interface BlogData {
   id: number;
   attributes: {
@@ -19,14 +21,36 @@ interface BlogData {
   };
 }
 
+const truncateString = (text: string, maxWords: number) => {
+  if (!text || typeof text !== "string") {
+    return "error occurred";
+  }
+
+  const words = text.split(" ");
+
+  if (words.length > maxWords) {
+    // Slice the array to the maximum number of words
+    const truncatedWords = words.slice(0, maxWords);
+
+    // Join string back
+    const truncatedText = truncatedWords.join(" ");
+
+    // Append ellipsis if the string is truncated
+    return `${truncatedText}...`;
+  }
+
+  return text;
+};
+
 const BlogList = () => {
   const [blogData, setBlogData] = useState<BlogData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        
-        const response = await axios.get("http://localhost:8082/api/blogs?populate=*");
+        const response = await axios.get(
+          "http://localhost:8082/api/blogs/?populate=*"
+        );
 
         setBlogData(response.data.data);
       } catch (error) {
@@ -36,6 +60,7 @@ const BlogList = () => {
 
     fetchData();
   }, []);
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="flex justify-center text-[2rem] p-2 w-[48%] mt-[73px] mb-[153px]">
@@ -44,17 +69,33 @@ const BlogList = () => {
           Tips, and Stories in Design, Marketing, and Web Development.
         </h1>
       </div>
-      <ul>
+      <div className="w-[80%] flex flex-col items-center gap-[42px] ">
         {blogData.map((blog) => (
-          <div key={blog.id}>
-            <li>
-              <h2>{blog.attributes.BlogTitle}</h2>
-              <p>{blog.attributes.BlogDescription}</p>
-              <img src= {blog.attributes.BlogThumbnail.data.attributes.name} alt="my image " />
-            </li>
+          <div key={blog.id} className="flex w-[90%] justify-center gap-[25px]">
+            <div className="">
+              {blog.attributes.BlogThumbnail && (
+                <StyledImg
+                  src={`http://localhost:8082${blog.attributes.BlogThumbnail.data.attributes.url}`}
+                  alt={blog.attributes.BlogThumbnail.data.attributes.name}
+                />
+              )}
+            </div>
+            <div className="w-[26%]">
+              <h2 className="text-utils text-[21px] font-bold">
+                {blog.attributes.BlogTitle}
+              </h2>
+              <h3 className="text-primary">{blog.attributes.BlogDate}</h3>{" "}
+              <StyledDes
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(
+                    truncateString(blog.attributes.BlogDescription, 58)
+                  ),
+                }}
+              />
+            </div>
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
