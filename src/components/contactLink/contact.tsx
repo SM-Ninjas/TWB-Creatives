c9f9e3cc013b82a8b354e0c5cd19b8feed10765c
sushant babu prasai <sushantprasaii7@gmail.com>
2024-02-05 15:25:35 +0545

    icon sized

--- src/components/contactLink/contact.tsx
+++ src/components/contactLink/contact.tsx
@@ -10,7 +10,7 @@ function ContactLinks() {
     <div className="fixed bottom-[5rem] right-5 flex flex-col items-end gap-4 z-50 bg-white p-4 rounded-md shadow-md">
     <div className="mb-2">
       <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
-        <Whatsapp size="48" color="#25d366" variant="Bold" />
+        <Whatsapp size="54" color="#25d366" variant="Bold" />
       </a>
     </div>
     <div className="mb-2">
